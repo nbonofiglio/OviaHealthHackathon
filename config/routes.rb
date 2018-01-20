02797bf932554d908a7aaf5906a2f4104867ec68
nanomanon56 <manon.l.jean@gmail.com>
2018-01-20 14:28:11 -0500

    more view pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  authenticated :user do
+   root 'pages#Sti', as: :authenticated_root
+ end
+
 get 'pages/Sexual_assult1'
 get 'pages/Sti'
 
