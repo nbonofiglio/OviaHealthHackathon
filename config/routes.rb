213525e87d32219195666a0607c96b06536e5e83
Margaryta Tchouikevitch <margaryta@gmail.com>
2018-01-20 16:09:15 -0500

    fixed conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-
-<<<<<<< HEAD
 root 'pages#home'
 get 'landing' => 'pages#landing'
 get 'pages/Sexual_assult1'
 get 'pages/Sti'
-=======
-
 get 'pages/Sexual_assault1'
   authenticated :user do
    root 'pages#Sti', as: :authenticated_root
  end
-
-
 get 'pages/Prenatal_sweden'
-
 get 'pages/landing'
 get 'pages/India_summary'
 get 'pages/Sweden_summary'
-
-
->>>>>>> 38af124e9e95d99869c112c9aa25f0f561074bba
-
-
 end
