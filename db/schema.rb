d106cb6207f5048b22796ef57fb8126c4dd3c907
Nick Bonofiglio <nicholas.bonofiglio@gmail.com>
2018-01-20 11:15:59 -0500

    updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180120151202) do
 
   create_table "users", force: :cascade do |t|
