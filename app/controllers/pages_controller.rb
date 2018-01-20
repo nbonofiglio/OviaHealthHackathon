a7a1df09392a0c19040551f7ad57675c066e4bdd
nanomanon56 <manon.l.jean@gmail.com>
2018-01-20 13:22:03 -0500

    manons changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,17 @@ class PagesController < ApplicationController
 	def home
 	end
 
+ def Sexual_assult1
+ end
+
+ def Sti
+ end
+
+	def Conerceptive_Plan_B
+	end
+
+   def Prenavite
+	 end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+get 'pages/Sexual_assult1'
+get 'pages/Sti'
+
 	root 'pages#home'
+
+
+  #
 
 end
